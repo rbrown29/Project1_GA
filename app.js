e74d140fef6ca220818423a1dbfb1a8cb2137816
Richard Brown <richardbrown@Richard.local>
2019-10-19 18:44:36 -0700

    git hub det up and initial code setup

---
+++ app.js
@@ -0,0 +1,40 @@
+console.log("this Works!");
+$(() => {
+console.log("here");
+
+        $.ajax({
+            url: "https://newsapi.org/v2/everything?q=bitcoin&apiKey=fed0a49b182549368f00fc7862d755e7"
+     
+        }).then(
+            (data)=>{
+              console.log(data);
+              $('#Author').html(data.articles[0].author);
+              $('#description').html(data.articles[0].description);  
+              $('#title').html(data.articles[0].title);
+              $('#webpage').html(data.articles[0].url);
+              $('#Author1').html(data.articles[1].author);
+              $('#description1').html(data.articles[1].description);  
+              $('#title1').html(data.articles[1].title);
+              $('#webpage1').html(data.articles[1].url);
+              $('#Author2').html(data.articles[2].author);
+              $('#description2').html(data.articles[2].description);  
+              $('#title2').html(data.articles[2].title);
+              $('#webpage2').html(data.articles[2].url);
+              $('#Author3').html(data.articles[3].author);
+              $('#description3').html(data.articles[3].description);  
+              $('#title3').html(data.articles[3].title);
+              $('#webpage3').html(data.articles[3].url);
+            },
+            ()=>{
+                console.log("") 
+            }
+        );
+
+
+   //});
+
+
+
+        
+    
+});
