a8df644e237c4af84e42fdb3811fea37542b5aaf
Richard Brown <richardbrown@Richard.local>
2019-10-22 19:09:57 -0700

    added clickable links to articles

--- app.js
+++ app.js
@@ -11,22 +11,45 @@ $(() => {
               $('#Author').html(data.articles[0].author);
               $('#description').html(data.articles[0].description);  
               $('#title').html(data.articles[0].title);
-              $('#webpage').html(data.articles[0].url);
+              const $atag = $("<a>");
+              $atag.attr("href", data.articles[0].url);
+              $atag.attr("target", "_blank");
+              $('#webpage').append($atag);
+              $atag.text(data.articles[0].url);
+              $atag.css("text-decoration", "none");
+        ////////////////////////////////////////////////////////////
               $('#Author1').html(data.articles[1].author);
               $('#description1').html(data.articles[1].description);  
               $('#title1').html(data.articles[1].title);
-              $('#webpage1').html(data.articles[1].url);
+              const $atag2 = $("<a>");
+              $atag2.attr("href", data.articles[1].url);
+              $atag2.attr("target", "_blank");
+              $('#webpage1').append($atag2);
+              $atag2.text(data.articles[1].url);
+              $atag2.css("text-decoration", "none");
+        ///////////////////////////////////////////////////////////////////
               $('#Author2').html(data.articles[2].author);
               $('#description2').html(data.articles[2].description);  
               $('#title2').html(data.articles[2].title);
-              $('#webpage2').html(data.articles[2].url);
+              const $atag3 = $("<a>");
+              $atag3.attr("href", data.articles[2].url);
+              $atag3.attr("target", "_blank");
+              $('#webpage2').append($atag3);
+              $atag3.text(data.articles[2].url);
+              $atag3.css("text-decoration", "none");
+        ///////////////////////////////////////////////////////////////////      
               $('#Author3').html(data.articles[3].author);
               $('#description3').html(data.articles[3].description);  
               $('#title3').html(data.articles[3].title);
-              $('#webpage3').html(data.articles[3].url);
+              const $atag4 = $("<a>");
+              $atag4.attr("href", data.articles[3].url);
+              $atag4.attr("target", "_blank");
+              $('#webpage3').append($atag4);
+              $atag4.text(data.articles[3].url);
+              $atag4.css("text-decoration", "none");
             },
             ()=>{
-                console.log("");
+                console.log("error");
             }
         );
 
@@ -39,7 +62,6 @@ $(() => {
          $("#dialog").css("visibility", "hidden");
          $("dl").css("visibility", "visible");
        });
-    
       
       $("form").on("submit", (event) => {
         event.preventDefault();
